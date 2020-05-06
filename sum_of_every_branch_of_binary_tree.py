e6bbd3c44e7ef3614632678bf2f39a06488a2df6
Nikolaos Mparoutis <baroutisnk@gmail.com>
2020-05-06 22:01:34 +0300

    Update sum_of_every_branch_of_binary_tree.py

--- sum_of_every_branch_of_binary_tree.py
+++ sum_of_every_branch_of_binary_tree.py
@@ -35,7 +35,7 @@ T(n) = 2*T(n/2) + f(1) = O(n) + O(1) = O(n) where n = number of nodes
 	Worst case where the tree is flattened to a list: 
 	O(n), n= number of node m = number of edges or keys.
 '''
-# Delete the prints for better code readability, they stayed for the grasp of concept. 
+# Delete the prints for better code readability, they stayed for the grasp of the concept and future reusability
 
 class BinaryTree:
     def __init__(self, value):
