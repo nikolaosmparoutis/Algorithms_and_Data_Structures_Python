ffdf8a3d2c811c62582c9dd29cfad1cb87a45ee7
baroutisnk@gmail.com <baroutisnk@gmail.com>
2020-07-04 02:47:08 +0300

    DRY testing and palindrome check on string in a maximum optimal way and elegant code.

---
+++ dry_unittest_palindrome_string.py
@@ -0,0 +1,25 @@
+import unittest
+from palindrome_string import isPalindrome
+
+# DRY approach using context manager on testing
+class MyTestCase(unittest.TestCase):
+    def expectEqual(self, first, second, msg=None):
+        with self.subTest():
+            self.assertEqual(first, second, msg)
+
+
+class TestPalindromeString(MyTestCase):
+    def test_palindrome(self):
+        self.assertEqual(isPalindrome(""), False)
+        self.assertEqual(isPalindrome("a"), True)
+        self.assertEqual(isPalindrome("ab"), False)
+        self.assertEqual(isPalindrome("aba"), True)
+        self.assertEqual(isPalindrome("abb"), False)
+        self.assertEqual(isPalindrome("abba"), True)
+        self.assertEqual(isPalindrome("abOba"), True)
+        self.assertEqual(isPalindrome("abOkba"), False)
+        self.assertEqual(isPalindrome("abcdcba"),True)
+        self.assertEqual(isPalindrome("abcdefghihgfeddcba"), False)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ palindrome_string.py
@@ -0,0 +1,21 @@
+# I found an elegant way to bypass the time O(N^2) or O(N) in
+# T(n) = O(n/2) | space O(1)
+# using the symmetry to search on the half of the input
+
+# Remeber that len and range need a marriage in types and
+# string slicing starts from 0 indexing while len do not.
+
+def isPalindrome(string):
+    lenStr = int(len(string))
+    # fundamental checks on string
+    if lenStr == 1:
+        return True
+    if lenStr == 0:
+        return False
+
+    for i in range(0, (lenStr // 2)):  # // is the lower floor division
+        if string[i] == string[lenStr - 1 - i]:
+            continue
+        else:
+            return False
+    return True
