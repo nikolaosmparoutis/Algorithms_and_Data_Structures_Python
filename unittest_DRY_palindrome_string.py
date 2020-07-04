20ad1f11c217135b632bac2168867b45569b3d35
baroutisnk@gmail.com <baroutisnk@gmail.com>
2020-07-04 04:29:26 +0300

    DRY testing_palindrome

--- unittest_DRY_palindrome_string.py
+++ unittest_DRY_palindrome_string.py
@@ -7,8 +7,8 @@ class MyTestCase(unittest.TestCase):
     def expectEqual(self, first, second, msg=None):
         with self.subTest():
             self.assertEqual(first, second, msg)
-JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ
-klklklklklk
+
+
 class TestPalindromeString(MyTestCase):
     def test_palindrome(self):
         self.assertEqual(isPalindrome(""), False)
